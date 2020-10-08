6da5f205295034785cce53e488ab5a71752c529a
Alan Hoffmeister <alanhoffmeister@gmail.com>
2020-10-08 19:04:17 -0300

    chore: fix build.rs

--- build.rs
+++ build.rs
@@ -8,16 +8,19 @@ fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
     println!("cargo:rerun-if-changed=build.rs");
 
+    let root = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     let bindings = bindgen::Builder::default()
-        .clang_arg(format!("-I{}", env::var("CARGO_MANIFEST_DIR").unwrap()))
+        .clang_arg(format!("-I{}", root.display()))
+        .clang_arg(format!("-I{}", root.join("include").display()))
         .header("wrapper.h")
         .generate_comments(true)
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(out.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }

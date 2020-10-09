78e6224b8ca5895ca906ce2a02ab1aa6ca88d46d
Alan Hoffmeister <alanhoffmeister@gmail.com>
2020-10-09 13:12:46 -0300

    feature: ship the headers with the source code

--- build.rs
+++ build.rs
@@ -8,10 +8,9 @@ fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
     println!("cargo:rerun-if-changed=build.rs");
 
-    let root = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 
     let bindings = bindgen::Builder::default()
-        .clang_arg(format!("-I{}", root.display()))
         .clang_arg(format!("-I{}", root.join("include").display()))
         .header("wrapper.h")
         .generate_comments(true)
